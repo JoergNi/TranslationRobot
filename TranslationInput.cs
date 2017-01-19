90c0280a37fc9b6504e2fc1ab2b09348422e8a54
vipa <vipa@DESKTOP-POQEFHR>
2017-01-19 15:55:58 +0100

    Added TranslationInput

---
+++ TranslationInput.cs
@@ -0,0 +1,10 @@
+namespace TranslationRobot
+{
+    public class TranslationInput
+    {
+        public string Input { get; set; }
+        public string Translation { get; set; }
+
+        public byte[] EncodedTranslation { get; set; }
+    }
+}
